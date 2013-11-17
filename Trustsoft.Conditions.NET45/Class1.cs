69d358acdf89b8f1f3301d510864999d998f364a
msukhanov <sukhanov.maxim@gmail.com>
2013-11-17 16:15:25 +0400

    ADD  Initial solution structure.

---
+++ Trustsoft.Conditions.NET45/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trustsoft.Conditions.NET45
+{
+    public class Class1
+    {
+    }
+}
