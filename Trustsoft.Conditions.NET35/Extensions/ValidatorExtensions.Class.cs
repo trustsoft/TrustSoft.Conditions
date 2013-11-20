178d93ef8a25a00595f08a052617d30351526d92
msukhanov <sukhanov.maxim@gmail.com>
2013-11-20 08:57:15 +0400

    UPD  Some code refactoring.

--- Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.Class.cs
+++ Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.Class.cs
@@ -1,8 +1,8 @@
 ﻿//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
-// <copyright file="ValidatorExtensions.Type.cs" company="Trustsoft Ltd.">
+// <copyright file="ValidatorExtensions.Class.cs" company="Trustsoft Ltd.">
 //     Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.
 // </copyright>
-// <date>18.11.2013</date>
+// <date>20.11.2013</date>
 //------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
 
 namespace Trustsoft.Conditions
@@ -17,6 +17,36 @@ namespace Trustsoft.Conditions
 
     public partial class ValidatorExtensions
     {
+        /// <summary>
+        ///     Checks whether the given value is null. An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the <see cref="IArgumentValidator{T}.Argument"> Value </see> of the specified
+        ///     <paramref name="validator"/>.
+        /// </typeparam>
+        /// <param name="validator"> The <see cref="IArgumentValidator{T}"/> that holds the value that has to be checked. </param>
+        /// <param name="conditionDescription">
+        ///     The description of the condition that should hold. The string may hold the placeholder '{0}' for
+        ///     the <see cref="IArgumentValidator{T}.Argument"> argument name </see>.
+        /// </param>
+        /// <returns> The specified <paramref name="validator"/> instance. </returns>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when the <see cref="IArgumentValidator{T}.Argument"> Value </see> of the
+        ///     specified <paramref name="validator"/> is not null, while the specified <paramref name="validator"/> is created
+        ///     using the <see cref="Requires.That{T}(T,string)"> Requires </see> extension method.
+        /// </exception>
+        public static IArgumentValidator<T> IsNull<T>(this IArgumentValidator<T> validator,
+                                                      string conditionDescription = null) where T : class
+        {
+            if (validator.Argument.Value != null)
+            {
+                string msg = MessageBuilder.Combine(validator, conditionDescription, StringRes.ValueShouldBeNull, false);
+                validator.Error.Handle(msg);
+            }
+
+            return validator;
+        }
+
         public static IArgumentValidator<T> IsOfType<T>(this IArgumentValidator<T> validator,
                                                         Type type,
                                                         string conditionDescription = null) where T : class
--- Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.cs
+++ Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.cs
@@ -7,47 +7,10 @@
 
 namespace Trustsoft.Conditions
 {
-    #region " Using Directives "
-
-    using System;
-
-    using Trustsoft.Conditions.Internals;
-
-    #endregion
-
     /// <summary>
     ///     Contains extension methods for <see cref="IArgumentValidator{T}"/>.
     /// </summary>
     public partial class ValidatorExtensions
     {
-        /// <summary>
-        ///     Checks whether the given value is null. An exception is thrown otherwise.
-        /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the <see cref="IArgumentValidator{T}.Argument"> Value </see> of the specified
-        ///     <paramref name="validator"/>.
-        /// </typeparam>
-        /// <param name="validator"> The <see cref="IArgumentValidator{T}"/> that holds the value that has to be checked. </param>
-        /// <param name="conditionDescription">
-        ///     The description of the condition that should hold. The string may hold the placeholder '{0}' for
-        ///     the <see cref="IArgumentValidator{T}.Argument"> argument name </see>.
-        /// </param>
-        /// <returns> The specified <paramref name="validator"/> instance. </returns>
-        /// <exception cref="ArgumentException">
-        ///     Thrown when the <see cref="IArgumentValidator{T}.Argument"> Value </see> of the
-        ///     specified <paramref name="validator"/> is not null, while the specified <paramref name="validator"/> is created
-        ///     using the <see cref="Requires.That{T}(T,string)"> Requires </see> extension method.
-        /// </exception>
-        public static IArgumentValidator<T> IsNull<T>(this IArgumentValidator<T> validator,
-                                                      string conditionDescription = null) where T : class
-        {
-            if (validator.Argument.Value != null)
-            {
-                string msg = MessageBuilder.Combine(validator, conditionDescription, StringRes.ValueShouldBeNull, false);
-                validator.Error.Handle(msg);
-            }
-
-            return validator;
-        }
     }
 }
