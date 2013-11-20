e49567b0af482839f238f500a7e22efc94fae0de
msukhanov <sukhanov.maxim@gmail.com>
2013-11-20 05:11:04 +0400

    ADD  Evaluate extension method and unit tests.

---
+++ Tests/Trustsoft.Conditions.UnitTests.NET35/ValidatorExtensions/EvaluateUnitTest.cs
@@ -0,0 +1,86 @@
+﻿//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+// <copyright file="Evaluate.UnitTest.cs" company="Trustsoft Ltd.">
+//     Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.
+// </copyright>
+// <date>20.11.2013</date>
+//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+
+namespace Trustsoft.Conditions.UnitTests.ValidatorExtensions
+{
+    #region " Using Directives "
+
+    using System;
+    using System.Linq.Expressions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    #endregion
+
+    // ReSharper disable InconsistentNaming
+    // ReSharper disable ExpressionIsAlwaysNull
+
+    [TestClass]
+    public class EvaluateUnitTest
+    {
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [Description("Calling Evaluate on 123 with expression 'x => x == 123' should pass.")]
+        public void Evaluate_ShouldPass_OnExpressionValue()
+        {
+            int a = 123;
+            Expression<Func<int, bool>> expression = x => x == 123;
+            Requires.That(a).Evaluate(expression);
+        }
+
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling Evaluate on 33 with expression '(x) => (x == 444)' should fail.")]
+        public void Evaluate_ShouldFail_OnExpressionValue()
+        {
+            int a = 3;
+            Requires.That(a).Evaluate(x => x == 4);
+        }
+
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [Description("Calling Evaluate on object x with expression 'x => true' should pass.")]
+        public void Evaluate_ShouldPass_OnObjectWithExpression1()
+        {
+            object a = new object();
+            Requires.That(a).Evaluate(x => true);
+        }
+
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Description("Calling Evaluate on null object x with expression 'x => x != null' should fail.")]
+        public void Evaluate_ShouldFail_OnNullWithExpression1()
+        {
+            object a = null;
+            Requires.That(a).Evaluate(x => x != null);
+        }
+
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Description("Calling Evaluate on null object x with expression 'x => x == 3' should fail.")]
+        public void Evaluate_ShouldFail_OnNullWithExpression2()
+        {
+            int? a = null;
+            Requires.That(a).Evaluate(x => x == 3);
+        }
+
+        [TestMethod]
+        [TestCategory("Evaluate")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling Evaluate with 'null' should fail.")]
+        public void Evaluate_ShouldFail_OnValueWithNull()
+        {
+            Requires.That(3).Evaluate(null);
+        }
+    }
+
+    // ReSharper restore ExpressionIsAlwaysNull
+    // ReSharper restore InconsistentNaming
+}
---
+++ Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.Evaluation.cs
@@ -0,0 +1,65 @@
+﻿//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+// <copyright file="ValidatorExtensions.Evaluation.cs" company="Trustsoft Ltd.">
+//     Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.
+// </copyright>
+// <date>20.11.2013</date>
+//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+
+namespace Trustsoft.Conditions
+{
+    #region " Using Directives "
+
+    using System;
+    using System.Linq.Expressions;
+
+    using Trustsoft.Conditions.Internals;
+
+    #endregion
+
+    public partial class ValidatorExtensions
+    {
+        /// <summary>
+        ///     Checks whether the specified <paramref name="expression"/> evaluates <b> true </b> on the given value.
+        ///     An exception is thrown otherwise.
+        /// </summary>
+        /// <remarks>
+        ///     This method will display a string representation of the specified <paramref name="expression"/>.
+        ///     Although it can therefore give a lot of useful information in the exception message, it the
+        ///     <paramref name="expression"/> has to be compiled on each call.
+        /// </remarks>
+        /// <typeparam name="T"> The type of the given value of the specified <paramref name="validator"/>. </typeparam>
+        /// <param name="validator"> The <see cref="IArgumentValidator{T}"/> that holds the value that has to be checked. </param>
+        /// <param name="expression">
+        ///     The <see cref="Expression{T}"/> that will be compiled to an <see cref="Func{T, TResult}"/> and
+        ///     executed. When the expression is a null reference it is considered to evaluate <b> false </b>.
+        /// </param>
+        /// <param name="conditionDescription"> The description of the condition that should hold. </param>
+        /// <returns> The specified <paramref name="validator"/> instance. </returns>
+        public static IArgumentValidator<T> Evaluate<T>(this IArgumentValidator<T> validator,
+                                                        Expression<Func<T, bool>> expression,
+                                                        string conditionDescription = null)
+        {
+            bool valueIsValid = false;
+
+            // Don't throw an ArgumentException when the expression is null, just consider it to be invalid.
+            if (expression != null)
+            {
+                Func<T, bool> func = expression.Compile();
+
+                valueIsValid = func(validator.Argument.Value);
+            }
+
+            if (!valueIsValid)
+            {
+                string msg = MessageBuilder.Combine(validator,
+                                                    conditionDescription,
+                                                    StringRes.LambdaXShouldHoldForValue,
+                                                    false,
+                                                    expression);
+                validator.Error.Handle(msg);
+            }
+
+            return validator;
+        }
+    }
+}
