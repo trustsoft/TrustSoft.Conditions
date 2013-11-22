cee07e7c806532899082462c440f85e96b02f03f
msukhanov <sukhanov.maxim@gmail.com>
2013-11-22 12:38:29 +0400

    ADD  A  few validation checks for IEnumerable and IEnumerable<T>.

---
+++ Tests/Trustsoft.Conditions.UnitTests.NET35/ValidatorExtensions/IEnumerable.UnitTest.cs
@@ -0,0 +1,313 @@
+﻿//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+// <copyright file="IEnumerable.UnitTest.cs" company="Trustsoft Ltd.">
+//     Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.
+// </copyright>
+// <date>22.11.2013</date>
+//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+
+namespace Trustsoft.Conditions.UnitTests.ValidatorExtensions
+{
+    #region " Using Directives "
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    #endregion
+
+    // ReSharper disable InconsistentNaming
+    // ReSharper disable ConvertToConstant.Local
+
+    [TestClass]
+    public class IEnumerableUnitTest
+    {
+        #region " IsEmpty "
+
+        [TestMethod]
+        [TestCategory("IEnumerable - IsEmpty")]
+        [Description("Calling IsEmpty on empty sequence should pass.")]
+        public void IsEmpty_ShouldPassOnEmptySequence()
+        {
+            IEnumerable seq = new int[0];
+
+            Requires.That(seq).IsEmpty();
+            Requires.That(seq, "seq").IsEmpty();
+            Requires.That(() => seq).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable<T> - IsEmpty")]
+        [Description("Calling IsEmpty on empty sequence should pass.")]
+        public void IsEmpty_ShouldPassOnEmptySequenceOf()
+        {
+            IEnumerable<int> seq = new int[0];
+
+            Requires.That(seq).IsEmpty();
+            Requires.That(seq, "seq").IsEmpty();
+            Requires.That(() => seq).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection - IsEmpty")]
+        [Description("Calling IsEmpty on empty collection should pass.")]
+        public void IsEmpty_ShouldPassOnEmptyCollection()
+        {
+            ICollection collection = new int[0];
+
+            Requires.That(collection).IsEmpty();
+            Requires.That(collection, "collection").IsEmpty();
+            Requires.That(() => collection).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection<T> - IsEmpty")]
+        [Description("Calling IsEmpty on empty collection should pass.")]
+        public void IsEmpty_ShouldPassOnEmptyCollectionOf()
+        {
+            ICollection<int> collection = new int[0];
+
+            Requires.That(collection).IsEmpty();
+            Requires.That(collection, "collection").IsEmpty();
+            Requires.That(() => collection).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList - IsEmpty")]
+        [Description("Calling IsEmpty on empty list should pass.")]
+        public void IsEmpty_ShouldPassOnEmptyList()
+        {
+            IList list = new int[0];
+
+            Requires.That(list).IsEmpty();
+            Requires.That(list, "list").IsEmpty();
+            Requires.That(() => list).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList<T> - IsEmpty")]
+        [Description("Calling IsEmpty on empty list should pass.")]
+        public void IsEmpty_ShouldPassOnEmptyListOf()
+        {
+            IList<int> list = new int[0];
+
+            Requires.That(list).IsEmpty();
+            Requires.That(list, "list").IsEmpty();
+            Requires.That(() => list).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty sequence should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptySequence()
+        {
+            IEnumerable seq = new[] { 8, 9, 0 };
+
+            Requires.That(seq).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable<T> - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty sequence should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptySequenceOf()
+        {
+            IEnumerable<int> seq = new[] { 8, 9, 0 };
+
+            Requires.That(seq, "seq").IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty collection should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptyCollection()
+        {
+            ICollection collection = new[] { 8, 9, 0 };
+
+            Requires.That(collection).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection<T> - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty collection should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptyCollectionOf()
+        {
+            ICollection<int> collection = new[] { 8, 9, 0 };
+
+            Requires.That(collection, "collection").IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty list should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptyList()
+        {
+            IList list = new[] { 8, 9, 0 };
+
+            Requires.That(() => list).IsEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList<T> - IsEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsEmpty on non-empty list should fail.")]
+        public void IsEmpty_ShouldFailOnNonEmptyListOf()
+        {
+            IList<int> list = new[] { 8, 9, 0 };
+
+            Requires.That(list, "list").IsEmpty();
+        }
+
+        #endregion
+
+        #region " IsNotEmpty "
+
+        [TestMethod]
+        [TestCategory("IEnumerable - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty sequence should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptySequence()
+        {
+            IEnumerable seq = new[] { 8, 9, 10 };
+
+            Requires.That(seq).IsNotEmpty();
+            Requires.That(seq, "seq").IsNotEmpty();
+            Requires.That(() => seq).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable<T> - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty sequence should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptySequenceOf()
+        {
+            IEnumerable<int> seq = new[] { 8, 9, 10 };
+
+            Requires.That(seq).IsNotEmpty();
+            Requires.That(seq, "seq").IsNotEmpty();
+            Requires.That(() => seq).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty collection should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptyCollection()
+        {
+            ICollection collection = new[] { 8, 9, 10 };
+
+            Requires.That(collection).IsNotEmpty();
+            Requires.That(collection, "collection").IsNotEmpty();
+            Requires.That(() => collection).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection<T> - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty collection should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptyCollectionOf()
+        {
+            ICollection<int> collection = new[] { 8, 9, 10 };
+
+            Requires.That(collection).IsNotEmpty();
+            Requires.That(collection, "collection").IsNotEmpty();
+            Requires.That(() => collection).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty list should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptyList()
+        {
+            IList list = new[] { 8, 9, 10 };
+
+            Requires.That(list).IsNotEmpty();
+            Requires.That(list, "list").IsNotEmpty();
+            Requires.That(() => list).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList<T> - IsNotEmpty")]
+        [Description("Calling IsNotEmpty on non-empty list should pass.")]
+        public void IsNotEmpty_ShouldPassOnEmptyListOf()
+        {
+            IList<int> list = new[] { 8, 9, 10 };
+
+            Requires.That(list).IsNotEmpty();
+            Requires.That(list, "list").IsNotEmpty();
+            Requires.That(() => list).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty sequence should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptySequence()
+        {
+            IEnumerable seq = new int[0];
+
+            Requires.That(seq).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IEnumerable<T> - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty sequence should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptySequenceOf()
+        {
+            IEnumerable<int> seq = new int[0];
+
+            Requires.That(seq, "seq").IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty collection should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptyCollection()
+        {
+            ICollection collection = new int[0];
+
+            Requires.That(collection).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("ICollection<T> - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty collection should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptyCollectionOf()
+        {
+            ICollection<int> collection = new int[0];
+
+            Requires.That(collection, "collection").IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty list should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptyList()
+        {
+            IList list = new int[0];
+
+            Requires.That(() => list).IsNotEmpty();
+        }
+
+        [TestMethod]
+        [TestCategory("IList<T> - IsNotEmpty")]
+        [ExpectedException(typeof(ArgumentException))]
+        [Description("Calling IsNotEmpty on empty list should fail.")]
+        public void IsNotEmpty_ShouldFailOnNonEmptyListOf()
+        {
+            IList<int> list = new int[0];
+
+            Requires.That(list, "list").IsNotEmpty();
+        }
+
+        #endregion
+    }
+
+    // ReSharper restore ConvertToConstant.Local
+    // ReSharper restore InconsistentNaming
+}
---
+++ Trustsoft.Conditions.NET35/Helpers/CollectionHelpers.cs
@@ -0,0 +1,84 @@
+﻿//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+// <copyright file="CollectionHelpers.cs" company="Trustsoft Ltd.">
+//     Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.
+// </copyright>
+// <date>21.11.2013</date>
+//------------------------Copyright © 2012-2013 Trustsoft Ltd. All rights reserved.------------------------
+
+namespace Trustsoft.Conditions
+{
+    #region " Using Directives "
+
+    using System.Collections;
+    using System.Collections.Generic;
+
+    #endregion
+
+    internal static class CollectionHelpers
+    {
+        #region " IsEmpty "
+
+        /// <summary>
+        ///     Determines whether a sequence contains no element.
+        /// </summary>
+        /// <param name="source"> A sequence in which to locate a value. </param>
+        /// <returns>
+        ///     <c> true </c> if specified sequence contains no element; otherwise <c> false </c>.
+        /// </returns>
+        public static bool IsEmpty(this IEnumerable source)
+        {
+            return !source.GetEnumerator().MoveNext();
+        }
+
+        /// <summary>
+        ///     Determines whether a sequence contains no element.
+        /// </summary>
+        /// <typeparam name="TSource"> The type of the elements of source. </typeparam>
+        /// <param name="source"> A sequence in which to locate a value. </param>
+        /// <returns>
+        ///     <c> true </c> if specified sequence contains no element; otherwise <c> false </c>.
+        /// </returns>
+        public static bool IsEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            using (var itr = source.GetEnumerator())
+            {
+                return !itr.MoveNext();
+            }
+        }
+
+        #endregion
+
+        #region " HasItems Operator "
+
+        /// <summary>
+        ///     Determines whether a sequence contains any item.
+        /// </summary>
+        /// <param name="source"> The sequence of elements. </param>
+        /// <returns> <c> false </c> if source is null or contains no items. </returns>
+        public static bool HasItems(this IEnumerable source)
+        {
+            return source != null && source.GetEnumerator().MoveNext();
+        }
+
+        /// <summary>
+        ///     Determines whether a sequence contains any item.
+        /// </summary>
+        /// <typeparam name="T"> Type of elements in <paramref name="source"/> sequence. </typeparam>
+        /// <param name="source"> The sequence of elements. </param>
+        /// <returns> <c> false </c> if source is null or contains no items. </returns>
+        public static bool HasItems<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                return enumerator.MoveNext();
+            }
+        }
+
+        #endregion
+    }
+}
