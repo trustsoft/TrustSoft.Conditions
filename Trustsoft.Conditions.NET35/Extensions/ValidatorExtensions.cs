341d418861393836d76151b1d7722b831b499ed7
msukhanov <sukhanov.maxim@gmail.com>
2013-11-25 00:42:10 +0400

    ADD  Special extension methods for IArgumentValidator<T>.

--- Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.cs
+++ Trustsoft.Conditions.NET35/Extensions/ValidatorExtensions.cs
@@ -9,7 +9,11 @@ namespace Trustsoft.Conditions
 {
     #region " Using Directives "
 
+    using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Linq;
+
+    using Trustsoft.Conditions.Internals;
 
     #endregion
 
@@ -20,5 +24,55 @@ namespace Trustsoft.Conditions
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static partial class ValidatorExtensions
     {
+        #region " GetErrors "
+
+        /// <summary>
+        ///     Gets all errors if any.
+        /// </summary>
+        /// <typeparam name="T"> The type of the given value of the specified <paramref name="validator"/>. </typeparam>
+        /// <param name="validator">
+        ///     The <see cref="IArgumentValidator{T}"/> that holds the value that has to
+        ///     be checked.
+        /// </param>
+        /// <returns> The list of errors. </returns>
+        public static IEnumerable<KeyValuePair<ViolationType, string>> GetErrors<T>(
+            this IArgumentValidator<T> validator)
+        {
+            var val = validator as CollectOnErrorValidator<T>;
+            if (val != null)
+            {
+                return val.GetHandler().Errors;
+            }
+
+            return Enumerable.Empty<KeyValuePair<ViolationType, string>>();
+        }
+
+        #endregion
+
+        #region " IsValid "
+
+        /// <summary>
+        ///     Checks whether all conditions are met.
+        /// </summary>
+        /// <typeparam name="T"> The type of the given value of the specified <paramref name="validator"/>. </typeparam>
+        /// <param name="validator">
+        ///     The <see cref="IArgumentValidator{T}"/> that holds the value that has to
+        ///     be checked.
+        /// </param>
+        /// <returns>
+        ///     <c> true </c> if all conditions was met; otherwise false.
+        /// </returns>
+        public static bool IsValid<T>(this IArgumentValidator<T> validator)
+        {
+            var val = validator as CollectOnErrorValidator<T>;
+            if (val != null)
+            {
+                return !val.GetHandler().Errors.Any();
+            }
+
+            return false;
+        }
+
+        #endregion
     }
 }
