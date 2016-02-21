6e770a647e01a3c4561cf878ad37dcd22e85278a
msukhanov <sukhanov.maxim@gmail.com>
2016-02-21 14:10:53 +0300

    ADD  Tests to check whether error messages composed correctly.

--- Tests/Trustsoft.Conditions.UnitTests.NET35/ValidatorExtensionsUnitTest.cs
+++ Tests/Trustsoft.Conditions.UnitTests.NET35/ValidatorExtensionsUnitTest.cs
@@ -8,25 +8,87 @@
 
     #endregion
 
+    // ReSharper disable ConvertToConstant.Local
+    // ReSharper disable ConditionIsAlwaysTrueOrFalse
+
     [TestClass]
     public class ValidatorExtensionsUnitTest
     {
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void Method1()
-        {
-            int value = 78;
-
-            Requires.That(value, "value").IsLessThan(34);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void Method2()
+        [TestCategory("ErrorMessage")]
+        [Description("Error message should be composed correctly.")]
+        public void ErrorMessageShouldBeComposedCorrectly1()
         {
             int value = 123;
 
-            Requires.That(value, "value").IsLessThan(56, "{name}({value}) must be less than {param1}");
+            try
+            {
+                Requires.That(value, "value").IsLessThan(56, "{name}({value}) must be less than {param1}");
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "value(123) must be less than 56");
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("ErrorMessage")]
+        [Description("Error message should be composed correctly.")]
+        public void ErrorMessageShouldBeComposedCorrectly2()
+        {
+            // Arrange
+            bool arg = false;
+
+            try
+            {
+                // Act/Assert
+                Requires.That(arg, "arg").IsTrue();
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "arg should be True");
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("ErrorMessage")]
+        [Description("Error message should be composed correctly.")]
+        public void ErrorMessageShouldBeComposedCorrectly3()
+        {
+            // Arrange
+            bool arg = false;
+
+            try
+            {
+                // Act/Assert
+                Requires.That(arg).IsTrue();
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "value should be True");
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("ErrorMessage")]
+        [Description("Error message should be composed correctly.")]
+        public void ErrorMessageShouldBeComposedCorrectly4()
+        {
+            // Arrange
+            bool arg = false;
+
+            try
+            {
+                // Act/Assert
+                Requires.That(() => arg).IsTrue();
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "arg should be True");
+            }
         }
     }
+
+    // ReSharper restore ConditionIsAlwaysTrueOrFalse
+    // ReSharper restore ConvertToConstant.Local
 }
