21ce3eb8b4db2705ee9a114be889c930da24c0e2
msukhanov <sukhanov.maxim@gmail.com>
2018-12-27 18:23:27 +0300

    UPD  Prepare for release 1.0.1.0.

--- Shared/CommonAssemblyInfo.cs
+++ Shared/CommonAssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
-[assembly: AssemblyInformationalVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
