979bf80dda2b4375718fda0e4563866d424334e2
m.sukhanov <sukhanov.maxim@gmail.com>
2024-12-28 21:58:33 +0300

    UPD  Rename net35 projects.

--- Tests/Trustsoft.Conditions.UnitTests/Properties/AssemblyInfo.cs
+++ Tests/Trustsoft.Conditions.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Trustsoft.Conditions.UnitTests.NET35")]
+[assembly: AssemblyTitle("Trustsoft.Conditions.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trustsoft.Conditions.UnitTests.NET35")]
+[assembly: AssemblyProduct("Trustsoft.Conditions.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
